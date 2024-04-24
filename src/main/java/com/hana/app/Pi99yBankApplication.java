cbcac8d5a3011ca17d020c436641d1ac7aad4fb2
bigstar9906 <bigstar9906@naver.com>
2024-04-24 15:05:38 +0900

    feat: Pi99yBank init

---
+++ src/main/java/com/hana/app/Pi99yBankApplication.java
@@ -0,0 +1,13 @@
+package com.hana.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi99yBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pi99yBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hana/app/Pi99yBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi99yBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
