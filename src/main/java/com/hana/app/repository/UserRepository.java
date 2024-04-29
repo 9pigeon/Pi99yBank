73f82e8182ef54c83f749cf4052132eb92c86a62
LeeJ <140700053+lee010207@users.noreply.github.com>
2024-04-29 17:34:03 +0900

    카카오 로그인 및 우대조건 화면 구현 (#7)

--- src/main/java/com/hana/app/repository/UserRepository.java
+++ src/main/java/com/hana/app/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.hana.app.repository;
 
-import com.hana.app.data.dto.UserDto;
+import com.hana.app.data.UserDto;
 import com.hana.app.frame.HanaRepository;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
-public interface UserRepository extends HanaRepository<String, UserDto>{
+public interface UserRepository extends HanaRepository<Long, UserDto> {
 }
