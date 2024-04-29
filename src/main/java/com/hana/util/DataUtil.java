76619a2664178d613112e1dea4dc1089db29615f
bigstar9906 <bigstar9906@naver.com>
2024-04-29 13:28:54 +0900

    feat: Saving Data API 호출 로직 + Test

---
+++ src/main/java/com/hana/util/DataUtil.java
@@ -0,0 +1,58 @@
+package com.hana.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hana.app.data.dto.SavingDto;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class DataUtil {
+
+    @Value(value = "${finlife.key}")
+    String key;
+
+    public List<SavingDto> getSavings() throws IOException, ParseException {
+        List<SavingDto> savings = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        log.info(key);
+        String sendUrl = "https://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth="+key+"&topFinGrpNo=020000&pageNo=1";
+        StringBuilder urlBuilder = new StringBuilder(sendUrl); /*URL*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        StringBuilder response = new StringBuilder();
+        while ((line = br.readLine()) != null) {
+            response.append(line);
+        }
+        JSONObject resJson = (JSONObject) new JSONParser().parse(response.toString());
+        JSONObject result = (JSONObject) resJson.get("result");
+        JSONArray baseList = (JSONArray) result.get("baseList");
+        for (int i=0;i<baseList.size();i++){
+            JSONObject obj = (JSONObject) baseList.get(i);
+            savings.add(objectMapper.readValue(obj.toString(), SavingDto.class));
+        }
+        log.info(baseList.toString());
+        br.close();
+        conn.disconnect();
+        return savings;
+    }
+}
---
+++ src/test/java/com/hana/app/DataUtilTest.java
@@ -0,0 +1,23 @@
+package com.hana.app;
+
+import com.hana.util.DataUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+@Slf4j
+public class DataUtilTest {
+    @Autowired
+    DataUtil dataUtil;
+    @Test
+
+    void contextLoads() throws IOException, ParseException {
+        log.info(dataUtil.getSavings().toString());
+    }
+}
